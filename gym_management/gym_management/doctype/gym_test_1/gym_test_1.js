e5170c4104d668a2a8b43fa64040fedd147c13e3
midhat <92074689+midhat2020@users.noreply.github.com>
2022-07-14 16:16:34 +0530

    Delete gym_management/gym_management/doctype/gym_test_1 directory
    As it is showing error

--- gym_management/gym_management/doctype/gym_test_1/gym_test_1.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, Midhat and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Gym Test 1', {
-	// refresh: function(frm) {
-
-	// }
-});
--- gym_management/gym_management/doctype/gym_test_1/test_gym_test_1.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2022, Midhat and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestGymTest1(FrappeTestCase):
-	pass
